45fb33fcb6845786b190af43ae2c263a62d6a250
alzog70 <zau23004@byui.edu>
2025-03-06 14:18:12 -0700

    Learning Shapes

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("Blue", 5);
+        shapes.Add(s1);
+
+        Circle s2 = new Circle("Green", 3);
+        shapes.Add(s2);
+
+        Rectangle s3 = new Rectangle("Purple", 3, 4);
+        shapes.Add(s3);
+
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+
+            double area = s.GetArea();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
